80daf1b4f865545b1cff63c6931db3c59fce388d
Alieva Nargiza <nargiza.alieva018@gmail.com>
2024-12-07 21:56:23 +0600

    Project start

---
+++ src/test/java/org/example/database_project/DatabaseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.database_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
